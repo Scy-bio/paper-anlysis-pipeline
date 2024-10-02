773f67f6c4d2041f41eb432e3143007d4c04df39
Scy-bio <65436100+Scy-bio@users.noreply.github.com>
2024-10-02 09:58:52 +0800

    Delete 01.GeneticDiversity directory

--- 01.GeneticDiversity/4.IBD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-##IBD
-beagle -Xmx60g -Djava.io.tmpdir=./tmp gt=all_snp.recode.vcf.gz  out=./out_beagle nthreads=9#phasing
-java -Xmx150g -jar /public/home/chcg/software/refined-ibd.17Jan20.102.jar   gt=./out_beagle.vcf.gz out=./out_beagle_phase.idb window=5e-3 trim=5e-6 lod=5 length=1e-5 
-
